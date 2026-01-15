87bbcaa08f0072493abb1579c0f29842075678bd
Sunjin Pak <spak2@csub.edu>
2026-01-14 16:20:14 -0800

    Initial commit: Peer Review Evaluation Survey

---
+++ teams.js
@@ -0,0 +1,78 @@
+// Team roster data for MGMT 4280 Spring 2026
+const SURVEY_CONFIG = {
+  course: "MGMT 4280 Current Topics in HRM (Spring 2026)",
+  apiUrl: "https://script.google.com/macros/s/AKfycbza6JLQFIFQGspBmTlm7oNrgf-R7lzNmBW24R4T_8MZy223aHDcLB5HI0VZ5jMrHdI--A/exec",
+
+  activities: [
+    "Team meeting 1",
+    "Team meeting 2",
+    "Team meeting 3",
+    "Team meeting 4",
+    "Team meeting 5",
+    "Team meeting 6",
+    "Team meeting 7",
+    "Team meeting 8",
+    "Team meeting 9",
+    "Team meeting 10",
+    "Team meeting 11",
+    "Team meeting 12",
+    "Team meeting 13",
+    "Team Presentation 1 - Case Discussion (Dates vary between teams)",
+    "Team Presentation 2 - Final Team Project (Dates vary between teams)"
+  ],
+
+  teams: [
+    {
+      number: 1,
+      members: ["Amelia Diaz", "Eric Perez", "Genesis Carrillo", "Guillermo Escobedo", "James Hamlin"]
+    },
+    {
+      number: 2,
+      members: ["Brissary Hinojosa Villarreal", "Elissa Arreola", "Katherine Soltero", "Monica Heredia", "Viviana Kaelin"]
+    },
+    {
+      number: 3,
+      members: ["Alejandro Ambriz", "Beatriz Salgado", "Janeth Marquez Ruiz", "Josephine Melendez", "Michelle Oliva"]
+    },
+    {
+      number: 4,
+      members: ["Alexis Headley", "Brianna Mcgill", "Jimena Carrillo Gomez", "Jorge Gonzalez", "Luka Geishauser"]
+    },
+    {
+      number: 5,
+      members: ["Esperanza Sanchez - Martinez", "Iliana Calderon", "Jesus Arellanes Gutierrez", "Jocelyn Picazo", "Natalie Portillo"]
+    },
+    {
+      number: 6,
+      members: ["Cyrina Perez", "Danielle Baeza", "Eduardo Meza", "Gracie Murdoch", "Tiana Flores"]
+    },
+    {
+      number: 7,
+      members: ["Allyson Loaisiga", "Esmeralda Ramirez", "Jackeline Gutierrez Hernandez", "John Austin Michael Saldana-Quezada", "Marissa Hernandez"]
+    },
+    {
+      number: 8,
+      members: ["Harrison Allred", "Jalyn Usher", "Jonathan Montoya", "Melanie Saldana", "Paul Rivas"]
+    },
+    {
+      number: 9,
+      members: ["Aliasgar Cassim", "Grecia Patino", "Jazmin Gallardo-Acevedo", "Lisa Bryant", "Saul Gallo"]
+    },
+    {
+      number: 10,
+      members: ["Andrea Vargas-Gonzalez", "Eryka Campos", "Ophelia Merchant", "Yoselyn Ortiz Gutierrez"]
+    },
+    {
+      number: 11,
+      members: ["Chad Brown", "Iveth Cruz", "Jaivir Garcha", "Yazil Duran"]
+    }
+  ],
+
+  ratingScale: [
+    { value: 1, label: "Little or weak effort", description: "Detrimental to team" },
+    { value: 2, label: "Insufficient effort", description: "Met minimal standards of team" },
+    { value: 3, label: "Sufficient effort", description: "Contributed adequately to team" },
+    { value: 4, label: "Very strong work", description: "Contributed significantly to team" },
+    { value: 5, label: "Excellent work", description: "Was crucial component to team's success" }
+  ]
+};
